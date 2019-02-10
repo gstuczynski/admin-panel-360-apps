79de830e4dcf7a24c25b2a2afd5832cf29264723
Grzegorz Stuczynski <stuczynski.g@gmail.com>
2019-02-10 23:18:00 +0100

    Rewirited to hooks

--- src/App.js
+++ src/App.js
@@ -1,51 +1,34 @@
-import React, { Component } from 'react';
-import { FilePond } from 'react-filepond';
-import axios from 'axios';
-import Gallery from 'react-grid-gallery';
+import React, { useState, useEffect } from "react";
+import { FilePond } from "react-filepond";
+import axios from "axios";
+import Gallery from "react-grid-gallery";
 
-import 'filepond/dist/filepond.min.css';
-import './App.css';
+import "filepond/dist/filepond.min.css";
+import "./App.css";
 
-const server_url = "https://gstuczynski.pl/api-360"
+const server_url = "https://gstuczynski.pl/api-360";
 
-class App extends Component {
-  constructor(){
-    super();
-    this.state = {
-      images: []
-    }
-  }
+const getImages = () =>
+  axios.get(`${server_url}/all-images`).then(res =>
+    res.data.map(img => ({
+      src: `https://gstuczynski.pl/images360/${img}`,
+      thumbnail: `https://gstuczynski.pl/images360/${img}`,
+      thumbnailWidth: 320,
+      thumbnailHeight: 212
+    }))
+  );
 
-  componentDidMount(){
-    this.getImages()
-  }
+export default () => {
+  const [items, setItems] = useState([]);
 
-  getImages(){
-   axios.get(`${server_url}/all-images`)
-      .then((res) => {
-        console.log(res)
-        this.setState({
-          images: res.data.map((img) => ({
-            src: `https://gstuczynski.pl/images360/${img}`,
-            thumbnail: `https://gstuczynski.pl/images360/${img}`,
-            thumbnailWidth: 320,
-            thumbnailHeight: 212
-          })
-             
-          )
-        })
-      })
-  }
-
-  render() {
-    return (
-      <div className="App">
-      {console.log()}
-        <FilePond server={`${server_url}/upload`} name="images"/>
-        <Gallery images={this.state.images}/>
-      </div>
-    );
-  }
-}
-
-export default App;
+  useEffect(async () => {
+    setItems(await getImages());
+  }, items);
+  return (
+    <div className="App">
+      {console.log(items)}
+      <FilePond server={`${server_url}/upload`} name="images" />
+      <Gallery images={items} />
+    </div>
+  );
+};
