c8ac6ed46107d6a67dc4aa77477c9cd1305567be
Grzegorz Stuczynski <stuczynski.g@gmail.com>
2019-02-10 22:07:21 +0100

    first commit; uploading files; showing gallery

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { FilePond } from 'react-filepond';
+import axios from 'axios';
+import Gallery from 'react-grid-gallery';
+
+import 'filepond/dist/filepond.min.css';
 import './App.css';
 
+const server_url = "https://gstuczynski.pl/api-360"
+
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      images: []
+    }
+  }
+
+  componentDidMount(){
+    this.getImages()
+  }
+
+  getImages(){
+   axios.get(`${server_url}/all-images`)
+      .then((res) => {
+        console.log(res)
+        this.setState({
+          images: res.data.map((img) => ({
+            src: `https://gstuczynski.pl/images360/${img}`,
+            thumbnail: `https://gstuczynski.pl/images360/${img}`,
+            thumbnailWidth: 320,
+            thumbnailHeight: 212
+          })
+             
+          )
+        })
+      })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      {console.log()}
+        <FilePond server={`${server_url}/upload`} name="images"/>
+        <Gallery images={this.state.images}/>
       </div>
     );
   }
